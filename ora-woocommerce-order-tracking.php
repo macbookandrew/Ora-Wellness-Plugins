059be5750040c05d4da0db1d8e732dfabe6a8e1f
Andrew Minion <andrew@andrewrminion.com>
2016-11-23 08:44:01 -0500

    run WooCommerce hooks on status change

--- ora-woocommerce-order-tracking.php
+++ ora-woocommerce-order-tracking.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: Ora WooCommerce Order Tracking
 Plugin URI: https://github.com/macbookandrew/Ora-Wellness-Plugins
-Description: Inegrates “Pending” and “Failed” into Infusionsoft
+Description: Inegrates “Pending” and “Failed” orders into Infusionsoft
 Version: 1.0
 Author: Andrew Minion/Pressed Solutions
 Author URI: http://www.pressedsolutions.com
@@ -12,7 +12,6 @@ Text Domain: genesis
 if (!defined('ABSPATH')) {
     exit;
 }
-
 
 /**
  * Register cron job on activation
@@ -44,6 +43,7 @@ add_action( 'ora_hourly_check', 'ora_failed_orders' );
 function ora_pending_orders() {
     $now = new DateTime( date( 'Y-m-d H:i:s' ) );
     $auto_failed_note = 'Failed due to payment pending for 1+ days.';
+    $new_status = 'wc-failed';
 
     // get all pending orders
     $pending_orders = wc_get_orders( array(
@@ -55,15 +55,19 @@ function ora_pending_orders() {
     foreach ( $pending_orders as $order ) {
         $order_date = new DateTime( $order->order_date );
         if ( $now->diff($order_date)->d >= 1 ) {
+            // get order and update order status
             $order = new WC_Order( $order->id );
-            $order->update_status( 'wc-failed', $auto_failed_note );
+            $old_status = $order->post_status;
+            $order->update_status( $new_status, $auto_failed_note );
+
+            // run WooCommerce hooks
+            do_action( 'woocommerce_order_status_' . $old_status . '_to_' . $new_status, $order->id );
+            do_action( 'woocommerce_order_status_changed', $order->id, $old_status, $new_status );
+
+            // add to notification list
+            $orders_failed[ $order->id ] = 'https://www.orawellness.com/wp-admin/post.php?action=edit&post=' . $order->id;
         }
     }
 }
 
-/**
- * Copy failed orders to Infusionsoft
- */
-function ora_failed_orders() {
-
-}
+//add_shortcode( 'dev_pending_orders', 'ora_pending_orders' );
